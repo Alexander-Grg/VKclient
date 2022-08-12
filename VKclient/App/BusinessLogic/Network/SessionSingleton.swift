2247fb35b944e072d095569b527d3ab78e4907b9
Alexander <a.a.grigoryev@icloud.com>
2022-08-12 12:26:35 +0300

    Code format edits.

--- VKclient/App/BusinessLogic/Network/SessionSingleton.swift
+++ VKclient/App/BusinessLogic/Network/SessionSingleton.swift
@@ -8,15 +8,11 @@
 import Foundation
 
 final class Session {
-
     static public let instance = Session()
-
     var token: String = ""
     var userID: Int = 0
-
     var friendID: Int = 0
     var photoIndex: Int = 0
 
     private init () {}
-
 }
