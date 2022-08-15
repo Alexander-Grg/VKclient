8ecf180bc0554574eb0adc77a283e42a0aa0d7c8
Alexander <a.a.grigoryev@icloud.com>
2022-08-15 18:38:57 +0300

    few edits

--- VKclient/App/BusinessLogic/Network/APIrequests.swift
+++ VKclient/App/BusinessLogic/Network/APIrequests.swift
@@ -17,7 +17,7 @@ final class GetFriends: NetworkService {
             URLQueryItem(
                 name: "fields",
                 value: "nickname, photo_100")
-           ])
+        ])
         dataTaskRequest { result in
             switch result {
             case .success(let data):
@@ -38,30 +38,30 @@ final class GetFriends: NetworkService {
             }
         }
     }
-
+    
 }
 
 final class GetPhotos: NetworkService {
     func request(_ friendID: Int,
-        completion: @escaping (Result<[RealmPhotos], RequestErrors>) -> Void) {
-            constructor.path += "photos.get"
-            constructor.queryItems?.append(contentsOf: [
-                URLQueryItem(
-                    name: "rev",
-                    value: "1"),
-                URLQueryItem(
-                    name: "album_id",
-                    value: "profile"),
-                URLQueryItem(
-                    name: "offset",
-                    value: "0"),
-                URLQueryItem(
-                    name: "photo_sizes",
-                    value: "0"),
-                URLQueryItem(
-                    name: "owner_id",
-                    value: String(friendID))
-             ])
+                 completion: @escaping (Result<[RealmPhotos], RequestErrors>) -> Void) {
+        constructor.path += "photos.get"
+        constructor.queryItems?.append(contentsOf: [
+            URLQueryItem(
+                name: "rev",
+                value: "1"),
+            URLQueryItem(
+                name: "album_id",
+                value: "profile"),
+            URLQueryItem(
+                name: "offset",
+                value: "0"),
+            URLQueryItem(
+                name: "photo_sizes",
+                value: "0"),
+            URLQueryItem(
+                name: "owner_id",
+                value: String(friendID))
+        ])
         dataTaskRequest { result in
             switch result {
             case .success(let data):
@@ -94,40 +94,40 @@ final class GetGroups: NetworkService {
                 URLQueryItem(
                     name: "fields",
                     value: "photo_100")
-              ])
-        dataTaskRequest { result in
-            switch result {
-            case .success(let data):
-                do {
-                    let groups = try JSONDecoder().decode(GroupsResponse.self,
-                                                          from: data).response.items
-                    let groupsRealm = groups.map { GroupsRealm(groups: $0)
+            ])
+            dataTaskRequest { result in
+                switch result {
+                case .success(let data):
+                    do {
+                        let groups = try JSONDecoder().decode(GroupsResponse.self,
+                                                              from: data).response.items
+                        let groupsRealm = groups.map { GroupsRealm(groups: $0)
+                        }
+                        
+                        DispatchQueue.main.async {
+                            try? RealmService.save(items: groupsRealm)
+                            completion(.success(groups))
+                        }
+                    } catch {
+                        print(completion(.failure(.decoderError)))
                     }
-
-                    DispatchQueue.main.async {
-                        try? RealmService.save(items: groupsRealm)
-                        completion(.success(groups))
-                    }
-                } catch {
-                    print(completion(.failure(.decoderError)))
+                case .failure(let error):
+                    completion(.failure(error))
                 }
-            case .failure(let error):
-                completion(.failure(error))
             }
         }
-    }
 }
 
 final class GetGroupSearch: NetworkService {
     func request(_ searchText: String,
-        completion: @escaping ([GroupsObjects]) -> Void) {
-            constructor.path += "groups.search"
-            constructor.queryItems?.append(contentsOf: [
-                URLQueryItem(name: "sort", value: "6"),
-                URLQueryItem(name: "type", value: "group"),
-                URLQueryItem(name: "q", value: searchText),
-                URLQueryItem(name: "count", value: "20")
-            ])
+                 completion: @escaping ([GroupsObjects]) -> Void) {
+        constructor.path += "groups.search"
+        constructor.queryItems?.append(contentsOf: [
+            URLQueryItem(name: "sort", value: "6"),
+            URLQueryItem(name: "type", value: "group"),
+            URLQueryItem(name: "q", value: searchText),
+            URLQueryItem(name: "count", value: "20")
+        ])
         dataTaskRequest { result in
             switch result {
             case .success(let data):
@@ -148,9 +148,9 @@ final class GetGroupSearch: NetworkService {
 }
 
 final class GetNews: NetworkService {
-
+    
     let dispatchGroup = DispatchGroup()
-
+    
     func request(startFrom: String = "", startTime: Double? = nil, _ completion: @escaping ([News], String)
                  -> Void) {
         constructor.path += "newsfeed.get"
@@ -166,39 +166,39 @@ final class GetNews: NetworkService {
             URLQueryItem(
                 name: "start_from",
                 value: startFrom))
-
+        
         if let startTime = startTime {
             constructor.queryItems?.append(URLQueryItem(name: "start_time", value: "\(startTime)"))
         }
-
+        
         dataTaskRequest { result in
             switch result {
             case .success(let data):
-
+                
                 var posts: [News] = []
                 var profiles: [User] = []
                 var groups: [Community] = []
                 var nextFrom = ""
-
+                
                 do {
                     let next: String = try JSONDecoder().decode(NewsResponse.self, from: data).response.nextFrom
                     nextFrom = next
-
+                    
                     DispatchQueue.global().async(group: self.dispatchGroup, qos: .userInitiated) {
                         let postJSON = try? JSONDecoder().decode(NewsResponse.self, from: data).response.items
                         posts = postJSON ?? []
                     }
-
+                    
                     DispatchQueue.global().async(group: self.dispatchGroup, qos: .userInitiated) {
                         let userJSON = try? JSONDecoder().decode(NewsResponse.self, from: data).response.profiles
                         profiles = userJSON ?? []
                     }
-
+                    
                     DispatchQueue.global().async(group: self.dispatchGroup, qos: .userInitiated) {
                         let groupsJSON = try? JSONDecoder().decode(NewsResponse.self, from: data).response.groups
                         groups = groupsJSON ?? []
                     }
-
+                    
                     self.dispatchGroup.notify(queue: DispatchQueue.global()) {
                         let newsWithSources = posts.compactMap { posts -> News? in
                             if posts.sourceId > 0 {
