08d15202989f67f7bd1a5e2f7008ab9e816bb1cb
Alexander <a.a.grigoryev@icloud.com>
2022-09-03 15:37:06 +0300

    GroupsFlow was transferred to the new architecture

--- VKclient/App/Flows/User/Presenter/FriendsFlowPresenter.swift
+++ VKclient/App/Flows/User/Presenter/FriendsFlowPresenter.swift
@@ -10,13 +10,10 @@ import Combine
 import RealmSwift
 import UIKit
 
-//Refers to View
 protocol FriendsFlowViewInput: AnyObject {
     func updateTableView()
-    func buttonDidPress()
 }
 
-//Refers to Presentor methods
 protocol FriendsFlowViewOutput: AnyObject {
     var dictOfUsers: [Character: [UserRealm]] { get }
     var firstLetters: [Character] { get }
@@ -38,27 +35,27 @@ final class FriendsFlowPresenter {
     weak var viewInput: (UIViewController & FriendsFlowViewInput)?
     
     // MARK: - Function for tableView sections
-     private func usersFilteredFromRealm(with friends: Results<UserRealm>?) {
-            self.dictOfUsers.removeAll()
-            self.firstLetters.removeAll()
-
-            if let filteredFriends = friends {
-                for user in filteredFriends {
-                    guard let dictKey = user.lastName.first else { continue }
-                    if var users = self.dictOfUsers[dictKey] {
-                        users.append(user)
-                        self.dictOfUsers[dictKey] = users
-                    } else {
-                        self.firstLetters.append(dictKey)
-                        self.dictOfUsers[dictKey] = [user]
-                    }
+    private func usersFilteredFromRealm(with friends: Results<UserRealm>?) {
+        self.dictOfUsers.removeAll()
+        self.firstLetters.removeAll()
+        
+        if let filteredFriends = friends {
+            for user in filteredFriends {
+                guard let dictKey = user.lastName.first else { continue }
+                if var users = self.dictOfUsers[dictKey] {
+                    users.append(user)
+                    self.dictOfUsers[dictKey] = users
+                } else {
+                    self.firstLetters.append(dictKey)
+                    self.dictOfUsers[dictKey] = [user]
                 }
-                self.firstLetters.sort()
             }
-         self.viewInput?.updateTableView()
+            self.firstLetters.sort()
         }
+        self.viewInput?.updateTableView()
+    }
     
-     private func fetchDataFromNetwork() {
+    private func fetchDataFromNetwork() {
         userService.requestUsers()
             .decode(type: UserResponse.self, decoder: JSONDecoder())
             .receive(on: DispatchQueue.main)
@@ -72,15 +69,15 @@ final class FriendsFlowPresenter {
             }
             )
             .store(in: &cancellable)
-   }
-   
-     private func savingDataToRealm(_ data: [UserObject]) {
-         do {
-             let dataRealm = data.map {UserRealm(user: $0)}
-             try RealmService.save(items: dataRealm)
-         } catch {
-             print("Saving to Realm failed")
-         }
+    }
+    
+    private func savingDataToRealm(_ data: [UserObject]) {
+        do {
+            let dataRealm = data.map {UserRealm(user: $0)}
+            try RealmService.save(items: dataRealm)
+        } catch {
+            print("Saving to Realm failed")
+        }
     }
     
     private func loadDataFromRealm() {
@@ -90,22 +87,22 @@ final class FriendsFlowPresenter {
             print("Download from Realm failed")
         }
     }
-
-     private func updatesFromRealm() {
-         self.notificationFriends = friendsFromRealm?.observe { [weak self] changes in
-           guard let self = self else { return }
-           switch changes {
-           case .initial:
-               break
-           case .update:
-               self.viewInput?.updateTableView()
-               self.loadDataFromRealm()
-               self.usersFilteredFromRealm(with: self.friendsFromRealm)
-           case let .error(error):
-               print(error)
-           }
-       }
-   }
+    
+    private func updatesFromRealm() {
+        self.notificationFriends = friendsFromRealm?.observe { [weak self] changes in
+            guard let self = self else { return }
+            switch changes {
+            case .initial:
+                break
+            case .update:
+                self.viewInput?.updateTableView()
+                self.loadDataFromRealm()
+                self.usersFilteredFromRealm(with: self.friendsFromRealm)
+            case let .error(error):
+                print(error)
+            }
+        }
+    }
     
     private  func filterFriends(with text: String) {
         guard !text.isEmpty else {
@@ -116,7 +113,7 @@ final class FriendsFlowPresenter {
         self.viewInput?.updateTableView()
     }
     
-      private func openFriendsPhotos(indexPath: IndexPath) {
+    private func openFriendsPhotos(indexPath: IndexPath) {
         let firstLetter = self.firstLetters[indexPath.section]
         if let users = self.dictOfUsers[firstLetter] {
             let userID = users[indexPath.row].id
