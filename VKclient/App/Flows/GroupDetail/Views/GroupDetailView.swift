79d7cd169f5d37b7dcef10b43fe2d408790cc907
Alexander Grigoryev <alx.a.grg@gmail.com>
2024-12-04 20:25:31 +0700

    Fix for the groups to update properly when the value in the API is changing

--- VKclient/App/Flows/GroupDetail/Views/GroupDetailView.swift
+++ VKclient/App/Flows/GroupDetail/Views/GroupDetailView.swift
@@ -12,19 +12,19 @@ protocol GroupDetailDelegate: AnyObject {
 }
 
 class GroupDetailView: UIView {
-
-//    MARK: Properties
+    
+    //    MARK: Properties
     weak var delegate: GroupDetailDelegate?
-
+    
     private(set) lazy var groupImage: UIImageView = {
         let image = UIImageView()
         image.translatesAutoresizingMaskIntoConstraints = false
         image.contentMode = .scaleAspectFit
         image.clipsToBounds = true
-
+        
         return image
     }()
-
+    
     private(set) lazy var groupNameLabel: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
@@ -32,10 +32,10 @@ class GroupDetailView: UIView {
         label.textAlignment = .center
         label.font = .systemFont(ofSize: 16, weight: .heavy)
         label.textColor = .black
-
+        
         return label
     }()
-
+    
     private(set) lazy var groupStatusLabel: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
@@ -43,10 +43,10 @@ class GroupDetailView: UIView {
         label.textAlignment = .center
         label.font = .systemFont(ofSize: 14, weight: .bold)
         label.textColor = .black
-
+        
         return label
     }()
-
+    
     private(set) lazy var isDeletedLabel: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
@@ -54,10 +54,10 @@ class GroupDetailView: UIView {
         label.textAlignment = .center
         label.font = .systemFont(ofSize: 14, weight: .bold)
         label.textColor = .black
-
+        
         return label
     }()
-
+    
     private(set) lazy var isMemberLabel: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
@@ -65,17 +65,17 @@ class GroupDetailView: UIView {
         label.textAlignment = .center
         label.font = .systemFont(ofSize: 14, weight: .bold)
         label.textColor = .black
-
+        
         return label
     }()
-
+    
     private(set) lazy var groupCoverImage: UIImageView = {
         let image = UIImageView()
         image.translatesAutoresizingMaskIntoConstraints = false
         image.contentMode = .scaleAspectFill
         return image
     }()
-
+    
     private(set) lazy var joinGroupButton: UIButton = {
         let button = UIButton(type: .system)
         button.translatesAutoresizingMaskIntoConstraints = false
@@ -84,11 +84,13 @@ class GroupDetailView: UIView {
         } else {
             // Fallback on earlier versions
         }
+        button.addTarget(self, action: #selector(joinTap), for: .touchUpInside)
+        
         return button
     }()
-
-//    MARK: Lifecycle
-
+    
+    //    MARK: Lifecycle
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         self.configureUI()
@@ -99,17 +101,17 @@ class GroupDetailView: UIView {
         self.configureUI()
         fatalError("init(coder:) has not been implemented")
     }
-
+    
     //    MARK: Methods
-
+    
     private func configureUI() {
         backgroundColor = .white
         self.addSubviews()
         self.setupConstraints()
     }
-
+    
     private func addSubviews() {
-//        self.addSubview(groupCoverImage)
+        //        self.addSubview(groupCoverImage)
         self.addSubview(groupImage)
         self.addSubview(groupNameLabel)
         self.addSubview(groupStatusLabel)
@@ -117,60 +119,54 @@ class GroupDetailView: UIView {
         self.addSubview(joinGroupButton)
         self.addSubview(isDeletedLabel)
     }
-
+    
     private func setupConstraints() {
         
         let s = safeAreaLayoutGuide
-
+        
         NSLayoutConstraint.activate([
-
-//            groupCoverImage.topAnchor.constraint(equalTo: s.topAnchor, constant: 20),
-//            groupCoverImage.leftAnchor.constraint(equalTo: s.leftAnchor),
-//            groupCoverImage.rightAnchor.constraint(equalTo: s.rightAnchor),
-
+            
+            //            groupCoverImage.topAnchor.constraint(equalTo: s.topAnchor, constant: 20),
+            //            groupCoverImage.leftAnchor.constraint(equalTo: s.leftAnchor),
+            //            groupCoverImage.rightAnchor.constraint(equalTo: s.rightAnchor),
+            
             groupImage.topAnchor.constraint(equalTo: s.topAnchor, constant: 20),
             groupImage.centerXAnchor.constraint(equalTo: s.centerXAnchor),
             groupImage.heightAnchor.constraint(equalToConstant: 100),
             groupImage.widthAnchor.constraint(equalToConstant: 100),
-
+            
             groupNameLabel.topAnchor.constraint(equalTo: groupImage.bottomAnchor),
             groupNameLabel.leftAnchor.constraint(equalTo: s.leftAnchor),
             groupNameLabel.rightAnchor.constraint(equalTo: s.rightAnchor),
             groupNameLabel.centerXAnchor.constraint(equalTo: groupImage.centerXAnchor),
             groupNameLabel.heightAnchor.constraint(equalToConstant: 25),
             groupNameLabel.widthAnchor.constraint(equalToConstant: 100),
-
+            
             groupStatusLabel.topAnchor.constraint(equalTo: groupNameLabel.bottomAnchor, constant: 10),
             groupStatusLabel.leftAnchor.constraint(equalTo: s.leftAnchor),
             groupStatusLabel.rightAnchor.constraint(equalTo: s.rightAnchor),
-
+            
             isMemberLabel.topAnchor.constraint(equalTo: groupStatusLabel.bottomAnchor, constant: 10),
             isMemberLabel.leftAnchor.constraint(equalTo: s.leftAnchor),
             isMemberLabel.rightAnchor.constraint(equalTo: s.rightAnchor),
-
+            
             joinGroupButton.topAnchor.constraint(equalTo: isMemberLabel.bottomAnchor, constant: 20),
             joinGroupButton.centerXAnchor.constraint(equalTo: s.centerXAnchor),
             joinGroupButton.heightAnchor.constraint(equalToConstant: 30),
             joinGroupButton.widthAnchor.constraint(equalToConstant: 200),
-
+            
             isDeletedLabel.topAnchor.constraint(equalTo: joinGroupButton.bottomAnchor, constant: 10),
             isDeletedLabel.leftAnchor.constraint(equalTo: s.leftAnchor),
             isDeletedLabel.rightAnchor.constraint(equalTo: s.rightAnchor)
         ])
     }
-
+    
     @objc func joinTap() {
         delegate?.didInviteTap(true)
     }
-
+    
     func setupJoinLeaveButton(isJoined: Bool) {
-        joinGroupButton.addTarget(self, action: #selector(joinTap), for: .touchUpInside)
-        joinGroupButton.setTitle("Join group", for: .normal)
-        joinGroupButton.setTitle("You're in the group", for: .disabled)
-        if isJoined {
-            joinGroupButton.isEnabled = false
-        } else {
-            joinGroupButton.isEnabled = true
-        }
+        joinGroupButton.setTitle(isJoined ? "You're in the group" : "Join group", for: .normal)
+        joinGroupButton.isEnabled = !isJoined
     }
 }
